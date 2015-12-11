4a3ddd449b77fdf891ee47a21e7c529591b0f335
Sam Rae <samuelrae@gmail.com>
2015-12-11 01:01:44 +0000

    made seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+prices = %w{1.09 2.44 3.66 4.99 5.99 6.00 7.25 8.45 9.99 10.00 11.00 12.67 13.89 14.99}
+names = %w{scissors hairbrush comb mirror tweezers stamps toothpaste shampoo conditioner doll ball string dye belt}
+
+names.each {|item| Product.create(name:item, net_price:0, vat: 20)}
+
+Product.all.each_with_index { |item, index| item.update({net_price:prices[index]}) }
+
+Product.all.each_with_index {|product, index| product.orders << Order.create(customer_name:"Joe Bloggs "+ index.to_s, order_date:Date.today, status:"DRAFT")}
+
+LineItem.all.each do |line_item|
+  line_item.update({quantity: 4})
+  line_item.updateTotals
+  line_item.updateProduct
+end
+
+Order.all.each {|order| order.updateTotal}
+

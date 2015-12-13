13f4ba285f12a795b4a9c8fe76ed68f836ed3936
Sam Rae <samuelrae@gmail.com>
2015-12-13 09:42:34 +0000

    added cascade:false to api.rb

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,6 +1,6 @@
 class API < Grape::API
   prefix 'api'
-  version 'v1', using: :path
+  version 'v1', using: :path, cascade: false
   mount Producttracker::Data
   mount Ordertracker::Data
   mount Lineitemtracker::Data
